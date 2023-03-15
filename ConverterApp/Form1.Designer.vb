a0df06e42c74507b46899f23e24426ff9e0afb92
Max <gowan_maxwell@columbusstate.edu>
2023-03-15 11:55:03 -0400

    Add project files.

---
+++ ConverterApp/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.lblEntry = New System.Windows.Forms.Label()
+        Me.txtEntry = New System.Windows.Forms.TextBox()
+        Me.grpSelection = New System.Windows.Forms.GroupBox()
+        Me.rbInchesToMeters = New System.Windows.Forms.RadioButton()
+        Me.rbMeterstoInches = New System.Windows.Forms.RadioButton()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpSelection.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 21)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(275, 213)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblHeader
+        '
+        Me.lblHeader.AutoSize = True
+        Me.lblHeader.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.Location = New System.Drawing.Point(344, 21)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(202, 31)
+        Me.lblHeader.TabIndex = 1
+        Me.lblHeader.Text = "Converter App"
+        '
+        'lblEntry
+        '
+        Me.lblEntry.AutoSize = True
+        Me.lblEntry.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEntry.Location = New System.Drawing.Point(305, 93)
+        Me.lblEntry.Name = "lblEntry"
+        Me.lblEntry.Size = New System.Drawing.Size(188, 48)
+        Me.lblEntry.TabIndex = 2
+        Me.lblEntry.Text = "Enter a value and " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "choose conversion"
+        '
+        'txtEntry
+        '
+        Me.txtEntry.BackColor = System.Drawing.Color.Gainsboro
+        Me.txtEntry.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEntry.Location = New System.Drawing.Point(526, 112)
+        Me.txtEntry.Name = "txtEntry"
+        Me.txtEntry.Size = New System.Drawing.Size(100, 29)
+        Me.txtEntry.TabIndex = 3
+        '
+        'grpSelection
+        '
+        Me.grpSelection.BackColor = System.Drawing.Color.Gainsboro
+        Me.grpSelection.Controls.Add(Me.rbMeterstoInches)
+        Me.grpSelection.Controls.Add(Me.rbInchesToMeters)
+        Me.grpSelection.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpSelection.Location = New System.Drawing.Point(350, 164)
+        Me.grpSelection.Name = "grpSelection"
+        Me.grpSelection.Size = New System.Drawing.Size(261, 131)
+        Me.grpSelection.TabIndex = 4
+        Me.grpSelection.TabStop = False
+        Me.grpSelection.Text = "Convert Measurement"
+        '
+        'rbInchesToMeters
+        '
+        Me.rbInchesToMeters.AutoSize = True
+        Me.rbInchesToMeters.Location = New System.Drawing.Point(7, 39)
+        Me.rbInchesToMeters.Name = "rbInchesToMeters"
+        Me.rbInchesToMeters.Size = New System.Drawing.Size(182, 28)
+        Me.rbInchesToMeters.TabIndex = 0
+        Me.rbInchesToMeters.TabStop = True
+        Me.rbInchesToMeters.Text = "Inches to Meters"
+        Me.rbInchesToMeters.UseVisualStyleBackColor = True
+        '
+        'rbMeterstoInches
+        '
+        Me.rbMeterstoInches.AutoSize = True
+        Me.rbMeterstoInches.Location = New System.Drawing.Point(7, 73)
+        Me.rbMeterstoInches.Name = "rbMeterstoInches"
+        Me.rbMeterstoInches.Size = New System.Drawing.Size(182, 28)
+        Me.rbMeterstoInches.TabIndex = 1
+        Me.rbMeterstoInches.TabStop = True
+        Me.rbMeterstoInches.Text = "Meters to Inches"
+        Me.rbMeterstoInches.UseVisualStyleBackColor = True
+        '
+        'btnConvert
+        '
+        Me.btnConvert.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnConvert.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.Location = New System.Drawing.Point(195, 366)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(92, 38)
+        Me.btnConvert.TabIndex = 5
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(371, 366)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(90, 38)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(536, 366)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(90, 38)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(345, 315)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(253, 25)
+        Me.lblResult.TabIndex = 8
+        Me.lblResult.Text = "This is the result Label"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.RosyBrown
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.grpSelection)
+        Me.Controls.Add(Me.txtEntry)
+        Me.Controls.Add(Me.lblEntry)
+        Me.Controls.Add(Me.lblHeader)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Building Plans Conversion"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpSelection.ResumeLayout(False)
+        Me.grpSelection.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents lblEntry As Label
+    Friend WithEvents txtEntry As TextBox
+    Friend WithEvents grpSelection As GroupBox
+    Friend WithEvents rbMeterstoInches As RadioButton
+    Friend WithEvents rbInchesToMeters As RadioButton
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblResult As Label
+End Class
---
+++ ConverterApp/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+    Private Sub txtEntry_TextChanged(sender As Object, e As EventArgs) Handles txtEntry.TextChanged
+
+    End Sub
+
+    Private Sub rbInchesToMeters_CheckedChanged(sender As Object, e As EventArgs) Handles rbInchesToMeters.CheckedChanged
+
+    End Sub
+
+    Private Sub rbMeterstoInches_CheckedChanged(sender As Object, e As EventArgs) Handles rbMeterstoInches.CheckedChanged
+
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        ' Check if the text box is empty or contains invalid input
+        If String.IsNullOrEmpty(txtEntry.Text) OrElse Not Double.TryParse(txtEntry.Text, Nothing) Then
+            MessageBox.Show("Please enter a valid measurement value.")
+        Else
+            Dim measurement As Double = Double.Parse(txtEntry.Text)
+            If measurement < 0 Then
+                ' Negative value entered
+                MessageBox.Show("Please enter a positive measurement value.")
+            ElseIf rbInchesToMeters.Checked Then
+                ' Convert Inches to Meters
+                Dim meters As Double = measurement / 39.37
+                lblResult.Text = $"{measurement} inches is {meters} meters"
+                lblResult.Visible = True
+            ElseIf rbMeterstoInches.Checked Then
+                ' Convert Meters to Inches
+                Dim inches As Double = measurement * 39.37
+                lblResult.Text = $"{measurement} meters is {inches} inches"
+                lblResult.Visible = True
+            Else
+                ' No RadioButton selected
+                MessageBox.Show("Please select a conversion option.")
+            End If
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear the text box and result label
+        lblResult.Text = ""
+        txtEntry.Text = ""
+
+        ' Set the Inches to Meters radio button as the default option
+        rbInchesToMeters.Checked = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblResult.Visible = False
+    End Sub
+
+    Private Sub lblEntry_Click(sender As Object, e As EventArgs) Handles lblEntry.Click
+
+    End Sub
+
+    Private Sub grpSelection_Enter(sender As Object, e As EventArgs) Handles grpSelection.Enter
+
+    End Sub
+End Class
